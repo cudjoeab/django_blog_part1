ebf681492ea2b1b3e528e5d37a62cfcccc0257d8
cudjoeab <cudjoeab@gmail.com>
2019-07-29 00:11:08 -0400

    made changes to blog to add articles through admin page

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from django.db import models 
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    draft = models.BooleanField()
+    published_date= models.DateTimeField(auto_now_add=True)
+    author= models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"title = {self.title}"
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 from django.http import HttpResponse, HttpResponseRedirect 
 from django.shortcuts import render
+from blog.models import Article 
 
 def root(request):
     return HttpResponseRedirect('/home')
 
 def home_page(request):
-    context = {'current_time': datetime.now()}
+    # context = {'current_time': datetime.now()}
 
+    context = { 'articles': Article.objects.all() }
     response = render(request, 'index.html', context)
     return HttpResponse(response)
 
