88b81d1037ca934add815435eb70df0892beb3f5
cudjoeab <cudjoeab@gmail.com>
2019-07-29 20:59:49 -0400

    added styling to the blog

--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,8 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     draft = models.BooleanField(default=True)
-    published_date=models.DateField()
+    published_date= models.DateTimeField(auto_now_add=True)
     author= models.CharField(max_length=255)
 
     def __str__(self):
-        return f"title = {self.title}"
+        return f(f'{self.title}, By: {self.author}') 
